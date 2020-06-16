8e68fa227670770849db9074f9a3903e11615b3b
ML-dev-all <mpdlemos@gmail.com>
2020-06-15 19:52:58 -0500

    Updates

---
+++ precache-manifest.5cb83f722e5c64277f8ab15f90d93432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705b32f777ccf9e4b38cc96de9a8db61",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "4bcc2ee25c889cf8f660",
+    "url": "/todoapp/static/css/main.935b1828.chunk.css"
+  },
+  {
+    "revision": "3d769eef2f4ea20e7bd8",
+    "url": "/todoapp/static/js/2.371af28f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.371af28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bcc2ee25c889cf8f660",
+    "url": "/todoapp/static/js/main.4fd24156.chunk.js"
+  },
+  {
+    "revision": "434477cfbc1f3b6c8fec",
+    "url": "/todoapp/static/js/runtime-main.64d7f731.js"
+  }
+]);
